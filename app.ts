85ed2af46367b549dbf29588011531ecd6e1c7f8
Chris Lankford <christopherlankford@gmail.com>
2016-03-28 10:03:38 -0700

    Added package.json and gitignore. Also added basic component to implement routerLink & router-outlet

--- app.ts
+++ app.ts
@@ -1,3 +1,25 @@
+import { Component } from 'angular2/core'
+import { ROUTER_DIRECTIVES } from 'angular2/router';
+import { RouteConfig } from 'angular2/router';
+
+@Component({
+    selector: 'router-app',
+    directives: [ROUTER_DIRECTIVES],
+    template: `
+    <div>
+        <nav>
+            <a>Navigation:</a>
+            <ul>
+                <li><a [routerLink]="['/Home']">Home</a></li>
+                <li><a [routerLink]="['/About']">About</a></li>
+                <li><a [routerLink]="['/Contact']">Contact us</a></li>
+            </ul>
+        </nav>
+        
+        <router-outlet></router-outlet>
+    </div>
+    `
+})
 @RouteConfig([
     { path: '/', name: 'root', redirectTo: ['/Home'] },
     { path: '/home', name: 'Home', component: HomeComponent },
