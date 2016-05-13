e7bb602e9758f355fbca0420337eee7b942423bb
Chris Lankford <christopherlankford@gmail.com>
2016-05-13 10:30:55 -0700

    Implemented the protected component

--- app/ts/components/ProtectedComponent.ts
+++ app/ts/components/ProtectedComponent.ts
@@ -5,8 +5,16 @@ import { AuthService } from '../services/AuthService';
 @Component({
     selector: 'protected',
     template: `
+    <h1>Protected content</h1>
     `
 })
+@CanActivate(
+    (nextInstr: any, currInstr: any) => {
+        let injector: any = ReflectiveInjector.resolveAndCreate([AuthService]);
+        let authService: AuthService = injector.get(AuthService);
+        return authService.isLogged();
+    }
+)
 export class ProtectedComponent {
     
 }
