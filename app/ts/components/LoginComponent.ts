edb7da7e3d0b2dc399c864eb8ffbf6d60baa06ba
Chris Lankford <christopherlankford@gmail.com>
2016-05-13 09:46:58 -0700

    Added the skeleton for a login component

---
+++ app/ts/components/LoginComponent.ts
@@ -0,0 +1,12 @@
+import { Component } from 'angular2/core';
+import { AuthService } from '../services/AuthService';
+
+@Component({
+    selector: 'login',
+    template: `
+    
+    `
+})
+export class LoginComponent {
+    
+}
